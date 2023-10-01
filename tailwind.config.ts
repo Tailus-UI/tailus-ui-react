b41f80aee7b02cb086a3d0f5c7ca526e3fff7b81
Meschac Irung <meschacirung@gmail.com>
2023-10-01 21:58:55 +0200

    add themer plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from 'tailwindcss'
-import plugin from 'tailwindcss/plugin'
-
-const shadowColor = "gray";
+import themer from '@tailus/themer'
 
 export default {
   content: [
@@ -15,7 +13,7 @@ export default {
             inherit: colors.inherit,
             current: colors.current,
             transparent: colors.transparent,
-            primary: colors.indigo,
+            primary: colors.lime,
             secondary: colors.purple,
             accent: colors.fuchsia,
             danger: colors.red,
@@ -67,106 +65,8 @@ export default {
       },
     },
   },
-plugins: [
-        plugin(function ({ addBase, theme, matchUtilities, addComponents }) {
-            addBase({
-              ":root": {
-                    "--ui-light-border-color" : theme("colors.gray.200"),
-                    "--ui-dark-border-color": theme("colors.gray.800"),
-                    "--ui-light-border-hover-color": theme("colors.gray.300"),
-                    "--ui-dark-border-hover-color": theme("colors.gray.700"),
-                    
-                  "--btn-border-radius": theme("borderRadius.full"),
-                   "--btn-border-width" : "1.5px",
-                    "--accordion-border-radius": theme("borderRadius.xl"),
-                    "--accordion-shadow" : theme("boxShadow.xl"),
-                    "--annonce-border-radius" : theme("borderRadius.full"),
-                    "--avatar-border-radius": theme("borderRadius.full"),
-                    "--avatar-status-position": theme("spacing.px"),
-                    "--card-border-radius": theme("borderRadius.3xl"),
-                    "--card-light-bg": theme("colors.gray.100"),
-                    "--card-dark-bg": theme("colors.gray.950"),
-                    "--card-padding": theme("spacing.8"),
-                    
-                    "--card-shadow": `0 10px 15px -3px var(--card-shadow-appearance), 0 4px 6px -4px var(--card-shadow-appearance)`,
-                    "--card-shadow-appearance" : `theme(colors.${shadowColor}.900 / var(--card-shadow-opacity))`,
-                    "--card-shadow-opacity": "7.5%",
-                    
-                    "--feedback-dark-bg": theme("colors.gray.950"),
-                    "--feedback-light-border-color": theme("colors.gray.200"),
-                    "--feedback-dark-border-color": theme("colors.gray.800"),
-                    "--flag-border-radius": theme("borderRadius.xl"),
-                    "--flag-padding": theme("spacing.6"),
-                    
-                    "--badge-border-radius": theme("borderRadius.full"),
-                        
-                    "--menu-border-radius": theme("borderRadius.2xl"),
-                    "--menu-light-bg-opacity": "90%",
-                    "--menu-dark-bg-opacity": theme("50%"),
-                    "--menu-dark-bg": "theme(colors.gray.950 / 80%)",
-                    "--menu-light-border-color": theme("colors.gray.200"),
-                    "--menu-dark-border-color": theme("colors.gray.800"),
-                    "--menu-light-backdrop-blur": "2rem",
-                    "--menu-dark-backdrop-blur": "2rem",
-
-                    "--separator-light-color": theme("colors.gray.200"),
-                    "--separator-dark-color": theme("colors.gray.800"),
-                    
-                    "--alert-border-radius": theme("borderRadius.2xl"),
-                    "--field-border-radius": theme("borderRadius.lg"),
-                
-                    "--switch-border-radius": theme("borderRadius.full"),
-                    
-                    "--select-dark-bg": theme("colors.gray.950"),
-                    
-                    "--tabs-light-bg": theme("colors.gray.100"),
-                    "--tabs-light-border-color": theme("colors.gray.200"),
-                    "--tabs-dark-border-color": theme("colors.gray.800"),
-                    "--tabs-border-radius": theme("borderRadius.xl"),
-                    "--tabs-dark-bg": theme("colors.gray.800"),
-                    "--tabs-indicator-light-bg": theme("colors.primary.600"),
-                    "--tabs-indicator-dark-bg": theme("colors.gray.950"),
-                    
-                    "--tabs-bottom-indicator-trigger-dark-bg": theme("colors.gray.800"),
-                    "--tabs-bottom-indicator-border-radius" : theme("borderRadius.md"),
-                    "--tabs-bottom-indicator-light-bg": theme("colors.primary.600"),
-                "--tabs-bottom-indicator-dark-bg": theme("colors.primary.400"),
-                    
-                "--tabs-soft-border-radius": theme("borderRadius.full"),
-                "--popover-border-radius": theme("borderRadius.xl"),
-                "--popover-padding": theme("spacing.6"),
-                
-                "--tooltip-border-radius": theme("borderRadius.md"),
-                "--toast-border-radius": theme("borderRadius.xl"),
-
-                "--field-light-bg": theme("colors.gray.100"),
-                "--field-light-focus-bg": theme("colors.gray.50"),
-                "--field-dark-bg": theme("colors.gray.800"),
-                "--field-dark-focus-bg": theme("colors.gray.900"),
-                "--field-light-border-color": theme("colors.gray.300"),
-                "--field-dark-border-color": theme("colors.gray.800"),
-                },
-            });
-          addComponents({
-            ".card-shadow": {
-              boxShadow: `var(--card-shadow)`
-            },
-            ".feedback-shadow": {
-              boxShadow: `var(--card-shadow)`
-            },
-            ".menu-shadow": {
-              boxShadow: `var(--card-shadow)`
-            },
-            ".alert-shadow": {
-              boxShadow: `var(--card-shadow)`
-            }
-          })
-            matchUtilities({
-                perspective: (value) => ({
-                    perspective: value,
-                }),
-            });
-        }),
-    ],
+  plugins: [
+    themer,
+  ]
 }  satisfies Config
 
