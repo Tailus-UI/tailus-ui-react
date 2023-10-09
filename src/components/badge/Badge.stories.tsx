57c06afe15d4e29b6f5334ec9a860426d8fe4c9a
glodielukose <glodielukose@gmail.com>
2023-10-09 19:27:23 +0200

    issue: #5 for this issue I created different variants for the badge components and I also added the different colors and sizes as requested in the issue.

--- src/components/badge/Badge.stories.tsx
+++ src/components/badge/Badge.stories.tsx
@@ -1,11 +1,69 @@
-import BadgeUI from "./Badge";
-import BadgeOutlinedVariant from "./Outlined";
-import BadgeSoftVariant from "./Soft";
+import type { Meta, StoryObj } from '@storybook/react';
+import { Badge } from "./Badge";
 
-export default {
-    component: BadgeUI,
-}
 
-export const Badge = () => <BadgeUI />;
-export const Soft = () => <BadgeSoftVariant />;
-export const Outlined = () => <BadgeOutlinedVariant />;
+const meta: Meta<typeof Badge> = {
+  title: 'Badge',
+  component: Badge,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'A customizable badge component for React applications.',
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    variant: { 
+      control: 'inline-radio', 
+      options: ['solid', 'soft', 'outlined'] 
+    },
+    colorVariant: { 
+      control: 'inline-radio', 
+      options: ["primary", "secondary", "accent", "danger", "success", "warning", "info", "gray",] 
+    },
+    size: { 
+        control: 'inline-radio', 
+        options: ["sm", "md", "lg"] 
+      },
+    children: { control: 'text' },
+    className: { control: 'text' }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+
+export const Solid: Story = {
+  args: {
+    variant: 'solid',
+    colorVariant: 'primary',
+    size: "sm",
+    children: "solid",
+    className: ""
+  },
+};
+
+export const Soft: Story = {
+  args: {
+    variant: 'soft',
+    colorVariant: 'primary',
+    children: "soft",
+    size: "sm",
+    className: ""
+  },
+};
+
+export const Oulined: Story = {
+  args: {
+    variant: 'outlined',
+    colorVariant: "primary",
+    size: "sm",
+    children: "outlined",
+    className: ""
+  },
+};
+
--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,9 +1,61 @@
-import { badge } from "@tailus/themer-badge"
+import { badge, outlinedBadge, softBadge, } from "@tailus/themer-badge"
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '../../lib/utils';
 
-const BadgeUI = () => (
-     <span className={badge.danger.sm} role="badge">
-        8
-    </span>
-)
+const variantsMap = {
+    solid: badge,
+    outlined: outlinedBadge,
+    soft: softBadge
+}
 
-export default BadgeUI
+const colorsMap = {
+    primary: "primary",
+    secondary: "secondary",
+    accent: "accent",
+    danger: "danger",
+    success: "success",
+    warning: "warning",
+    info: "info",
+    gray: "gray",
+}
+
+const sizesMap = {
+    sm: "sm",
+    md: "md",
+    lg: "lg",
+}
+
+const badgeui = cva([''], {
+    variants: {
+      variant: variantsMap ,
+      colorVariant: colorsMap,
+      size: sizesMap,
+    },
+    defaultVariants: {
+        variant: 'outlined',
+        colorVariant: 'primary',
+        size: 'md'
+    }
+  });
+
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeui> {}
+
+export const Badge: React.FC<BadgeProps> = ({
+    className,
+    variant,
+    colorVariant,
+    size,
+    children,
+    ...props
+  }) => {
+   
+    const badgeClass = variant === "soft" ? softBadge[colorVariant!][size!] : variant === "outlined" ? outlinedBadge[colorVariant!][size!] : badge[colorVariant!][size!]
+    console.log(badgeClass)
+    const classes = cn(badgeui({ className }), badgeClass);
+    return(
+        <span  className={classes} {...props} role="badge">
+            {children}
+        </span>
+    )
+  }
+
