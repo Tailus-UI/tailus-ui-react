518fe52e4bc6cac3ce295acbdda49fc3e615ab01
glodielukose <glodielukose@gmail.com>
2023-10-10 20:20:55 +0200

    issue: #3 I've added the possibility to put a title and a message for the ad component and also the possibility to have an a element only when you have a href and in the contrary case to put a div

--- src/components/annonce/Annonce.stories.tsx
+++ src/components/annonce/Annonce.stories.tsx
@@ -23,6 +23,15 @@ const meta: Meta<typeof Annonce> = {
       control: 'select', 
       options: ["primary", "secondary", "accent", "danger", "success", "warning", "info", "gray",] 
     },
+    concern: {
+      control: 'text',
+    },
+    message: {
+      control: 'text',
+    },
+    href: {
+      control: 'text',
+    }
   }
 };
 
@@ -34,6 +43,9 @@ export const Oulined: Story = {
     args: {
       variant: 'outlined',
       colorVariant: "primary",
+      concern: "New",
+      message: "This is a new feature",
+      href: "https://www.google.com",
     },
 };
 
@@ -41,6 +53,9 @@ export const Soft: Story = {
   args: {
     variant: 'soft',
     colorVariant: 'primary',
+    concern: "New",
+    message: "This is a new feature",
+    href: "https://www.google.com",
   },
 };
 
--- src/components/annonce/Annonce.tsx
+++ src/components/annonce/Annonce.tsx
@@ -3,47 +3,67 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
 const variantsMap = {
-    outlined: annonce,
-    soft: softAnnonce
+  outlined: annonce,
+  soft: softAnnonce
 }
 
 const colorsMap = {
-    primary: "primary",
-    secondary: "secondary",
-    success: "success",
-    danger: "danger",
-    warning: "warning",
-    accent: "accent",
-    gray: "gray",
-    neutral: "neutral"
+  primary: "primary",
+  secondary: "secondary",
+  success: "success",
+  danger: "danger",
+  warning: "warning",
+  accent: "accent",
+  gray: "gray",
+  neutral: "neutral"
 }
 
-const AnnonceVariant = cva([''], {
-    variants: {
-      variant: variantsMap ,
-      colorVariant: colorsMap,
-    },
-    defaultVariants: {
-        variant: 'outlined',
-        colorVariant: 'primary',
-    }
+const annonceVariants = cva([''], {
+  variants: {
+    variant: variantsMap ,
+    colorVariant: colorsMap,
+  },
+  defaultVariants: {
+    variant: 'outlined',
+    colorVariant: 'primary',
+  }
   });
 
-export interface AnnonceProps extends React.HTMLAttributes<HTMLAnchorElement>, VariantProps<typeof AnnonceVariant> {}
+export interface AnnonceProps extends React.HTMLAttributes<HTMLElement>, VariantProps<typeof annonceVariants> {
+  message: string;
+  concern: string;
+  href?: string;
+}
 
 export const Annonce: React.FC<AnnonceProps> = ({
     className,
     variant,
     colorVariant,
+    concern,
+    message,
+    href,
     ...props
   }) => {
    
     const annonceUtilities = variantsMap[variant!].root
-    const classes = cn(AnnonceVariant({ className }), annonceUtilities);
+    const classes = cn(annonceVariants({ className }), annonceUtilities);
     return(
-        <a href="" className={classes} {...props}>
-            <span className={variantsMap[variant!].concern[colorVariant!]}>New</span>
-            <span className={variantsMap[variant!].message}>Introducing a all new Tailus !</span>
-        </a>
+        <>
+        {
+          href ? (
+            <a href={href} className={classes} {...props}>
+              <span className={variantsMap[variant!]['concern'][colorVariant!]}>{concern}</span>
+              <span className={variantsMap[variant!]['message']}>{message}</span>
+            </a>
+          ) : (
+            <div className={classes} {...props}>
+
+              <span className={variantsMap[variant!]['concern'][colorVariant!]}>{concern}</span>
+              <span className={variantsMap[variant!]['message']}>{message}</span>
+            </div>
+          )
+        }
+        </>
     )
   }
+
