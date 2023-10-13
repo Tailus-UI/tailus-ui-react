a70b963133fb8f50fe348c63b7373db72c7be3e0
glodielukose <glodielukose@gmail.com>
2023-10-13 10:12:20 +0200

    issue: #8 I just added ring and default variants for this component

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,39 +1,45 @@
-import * as Checkbox from "@radix-ui/react-checkbox";
-import {checkbox as checkboxTheme} from "@tailus/themer-checkbox";
+import * as CheckboxUi from "@radix-ui/react-checkbox";
+import {checkbox, ringVariant } from "@tailus/themer-checkbox";
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '../../lib/utils';
 
-const CheckboxUI = () => (
-    <div className="flex items-center">
-        <Checkbox.Root className={checkboxTheme.root} defaultChecked id="c1">
-            <Checkbox.Indicator className={checkboxTheme.indicator}>
-                <svg className="w-4 h-4" viewBox="0 0 46 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_23_2)">
-<g filter="url(#filter0_d_23_2)">
-<path d="M6 15.91L10.4241 11.546L19.2713 20.273L34.7547 5L39.1788 9.364L19.2723 29L6 15.91Z" fill="white"/>
-</g>
-</g>
-<defs>
-<filter id="filter0_d_23_2" x="2" y="4" width="41.1788" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="3"/>
-<feGaussianBlur stdDeviation="2"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.4 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_23_2"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_23_2" result="shape"/>
-</filter>
-<clipPath id="clip0_23_2">
-<rect width="46" height="33" fill="white"/>
-</clipPath>
-</defs>
-</svg>
+const variantsMap = {
+    default: checkbox,
+    ring: ringVariant,
+}
 
-            </Checkbox.Indicator>
-        </Checkbox.Root>
-        <label className={checkboxTheme.label} htmlFor="c1">
-            Keep me signed in
-        </label>
-    </div>
-);
+const checkboxVariants = cva([''], {
+    variants: {
+      variant: variantsMap ,
+    },
+    defaultVariants: {
+        variant: 'default',
+    }
+});
 
-export default CheckboxUI;
+export interface CheckboxProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof checkboxVariants> {}
+
+export const Checkbox: React.FC<CheckboxProps> = ({
+    className,
+    variant,
+    children,
+    ...props
+  }) => {
+    
+    const classes = cn('flex items-center', className)
+    return(
+        <div className={classes} {...props}>
+            <CheckboxUi.Root className={variantsMap[variant!].root} defaultChecked id="c1">
+                <CheckboxUi.Indicator className={variantsMap[variant!].indicator}>
+                    <svg className="h-3 w-3" viewBox="0 0 33 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 10.909L4.364 6.54499L13.091 15.272L28.364 -0.00100708L32.728 4.36299L13.092 23.999L0 10.909Z" fill="currentColor" />
+                    </svg>
+                </CheckboxUi.Indicator>
+            </CheckboxUi.Root>
+            <label className={variantsMap[variant!].label} htmlFor="c1">
+                {children}
+            </label>
+        </div>
+    )
+  }
+
