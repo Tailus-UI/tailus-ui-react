825501c463340964eb3c925995a9e9bf15f66732
Théo Balick <balickmethens@gmail.com>
2023-11-08 18:51:15 +0100

    Add preview to storybook

--- src/components/tooltip/Tooltip.stories.tsx
+++ src/components/tooltip/Tooltip.stories.tsx
@@ -1,9 +1,48 @@
-import TooltipUI from "./Tooltip";
-import TooltipArrow from "./WithArrow";
+import {TooltipArrow, TooltipContent, TooltipPortal, TooltipProvider, TooltipRoot, TooltipTrigger} from "./Tooltip";
+import {ArchiveIcon} from "@radix-ui/react-icons";
+import {iconButton as buttonTheme} from "@tailus/themer-button";
+
+export const Tooltip = () => {
+    return (
+      <TooltipProvider>
+          <TooltipRoot>
+              <TooltipTrigger>
+                  <button className={buttonTheme.warning.md}>
+                      <span className="sr-only">Archive</span>
+                      <ArchiveIcon className={buttonTheme.icon.md} aria-hidden/>
+                  </button>
+              </TooltipTrigger>
+              <TooltipPortal>
+                  <TooltipContent>
+                      Archive
+                  </TooltipContent>
+              </TooltipPortal>
+          </TooltipRoot>
+      </TooltipProvider>
+    );
+};
 
 export default {
-    component : TooltipUI,
+    component : Tooltip,
 }
 
-export const Tooltip = () => <TooltipUI />;
-export const WithArrow = () => <TooltipArrow />;
+export const WithArrow = () => {
+    return (
+      <TooltipProvider>
+          <TooltipRoot>
+              <TooltipTrigger>
+                  <button className={buttonTheme.warning.md}>
+                      <span className="sr-only">Archive</span>
+                      <ArchiveIcon className={buttonTheme.icon.md} aria-hidden/>
+                  </button>
+              </TooltipTrigger>
+              <TooltipPortal>
+                  <TooltipContent>
+                      Archive
+                        <TooltipArrow />
+                  </TooltipContent>
+              </TooltipPortal>
+          </TooltipRoot>
+      </TooltipProvider>
+    );
+}
