18d12c0fa03dd38dffa8792b42ad278247f3f56b
Théo Balick <balickmethens@gmail.com>
2023-11-09 15:20:28 +0100

    Add story to storybook [Tooltip component]

--- src/components/tooltip/Tooltip.stories.tsx
+++ src/components/tooltip/Tooltip.stories.tsx
@@ -1,8 +1,9 @@
 import {TooltipArrow, TooltipContent, TooltipPortal, TooltipProvider, TooltipRoot, TooltipTrigger} from "./Tooltip";
 import {ArchiveIcon} from "@radix-ui/react-icons";
 import {iconButton as buttonTheme} from "@tailus/themer-button";
+import {Meta, StoryObj} from "@storybook/react";
 
-export const Tooltip = () => {
+const TooltipUI = ({ withArrow }: { withArrow?: boolean }) => {
     return (
       <TooltipProvider>
           <TooltipRoot>
@@ -13,8 +14,9 @@ export const Tooltip = () => {
                   </button>
               </TooltipTrigger>
               <TooltipPortal>
-                  <TooltipContent>
+                  <TooltipContent className="bg-red-400">
                       Archive
+                      {withArrow && <TooltipArrow />}
                   </TooltipContent>
               </TooltipPortal>
           </TooltipRoot>
@@ -22,27 +24,29 @@ export const Tooltip = () => {
     );
 };
 
-export default {
-    component : Tooltip,
-}
+const meta: Meta<typeof TooltipUI> = {
+    title: 'Tooltip',
+    component: TooltipUI,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: 'A customizable button component for React applications.',
+            },
+        },
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        withArrow: { control: 'boolean', defaultValue: false  },
+    }
+};
 
-export const WithArrow = () => {
-    return (
-      <TooltipProvider>
-          <TooltipRoot>
-              <TooltipTrigger>
-                  <button className={buttonTheme.warning.md}>
-                      <span className="sr-only">Archive</span>
-                      <ArchiveIcon className={buttonTheme.icon.md} aria-hidden/>
-                  </button>
-              </TooltipTrigger>
-              <TooltipPortal>
-                  <TooltipContent>
-                      Archive
-                        <TooltipArrow />
-                  </TooltipContent>
-              </TooltipPortal>
-          </TooltipRoot>
-      </TooltipProvider>
-    );
-}
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Tooltip: Story = {
+    args: {
+        withArrow: true,
+    },
+};
