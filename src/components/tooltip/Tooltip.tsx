ad7b33fa985a4605b424df30e056ea08c4fe8fd2
Théo Balick <balickmethens@gmail.com>
2023-11-09 15:19:22 +0100

    Add style merge function [Tooltip component]

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,5 +1,6 @@
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
-import { tooltip as theme } from "@tailus/themer-tooltip";
+import {tooltip as theme} from "@tailus/themer-tooltip";
+import {cn} from "../../lib/utils";
 import React from "react";
 
 const TooltipProvider = TooltipPrimitive.Provider;
@@ -7,17 +8,31 @@ const TooltipRoot = TooltipPrimitive.Root;
 const TooltipTrigger = TooltipPrimitive.Trigger;
 const TooltipPortal = TooltipPrimitive.Portal;
 
-const TooltipContent = React.forwardRef((props, forwardedRef, sideOffset = 4) => (
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({className, sideOffset = 4, ...props}, ref) => (
   <TooltipPrimitive.Content
-    className={theme.content}
     sideOffset={sideOffset}
+    className={cn(theme.content, className)}
+    ref={ref}
+    {...props}
+  />
+))
+
+const TooltipArrow = React.forwardRef((props, forwardedRef) => (
+  <TooltipPrimitive.Arrow
+    className={theme.arrow}
     ref={forwardedRef}
     {...props}
   />
 ));
 
-const TooltipArrow = React.forwardRef((props, forwardedRef) => (
-  <TooltipPrimitive.Arrow className={theme.arrow} ref={forwardedRef} {...props} />
-));
-
-export { TooltipProvider, TooltipRoot, TooltipTrigger, TooltipPortal, TooltipContent, TooltipArrow };
+export {
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+  TooltipPortal,
+  TooltipContent,
+  TooltipArrow
+};
