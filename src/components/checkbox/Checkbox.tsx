1abca49bcefe0d79bd0dd3af1cf0648902b6ce8f
Théo Balick <balickmethens@gmail.com>
2023-11-09 21:32:05 +0100

    Customize checkbox component parts

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,64 +1,89 @@
-import * as CheckboxUi from "@radix-ui/react-checkbox";
-import {checkbox, ringVariant } from "@tailus/themer-checkbox";
-import { cva, VariantProps } from 'class-variance-authority';
-import { cn } from '../../lib/utils';
-import { useState } from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import {checkbox as checkboxTheme, ringVariant as checkBoxRingTheme} from "@tailus/themer-checkbox";
+import React, {forwardRef} from "react";
+import {cn} from "../../lib/utils";
+import {cva, type VariantProps} from "class-variance-authority";
 
-const variantsMap = {
-    default: checkbox,
-    ring: ringVariant,
-}
-
-const checkboxVariants = cva([''], {
-    variants: {
-      variant: variantsMap ,
-    },
-    defaultVariants: {
-        variant: 'default',
+const checkboxRoot = cva(checkboxTheme.root, {
+  variants: {
+    variant: {
+      checkbox: checkboxTheme.root,
+      ring: checkBoxRingTheme.root,
     }
+  },
 });
 
-export interface CheckboxProps extends React.HTMLAttributes<HTMLInputElement>, VariantProps<typeof checkboxVariants> {
-    typeOfChecked: 'checked' | 'defaultChecked'
+export interface CheckboxProps
+  extends VariantProps<typeof checkboxRoot> {
+  className?: string;
+  props?: React.ComponentProps<typeof CheckboxPrimitive.Root>;
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({
-    className,
-    variant,
-    children,
-    id,
-    typeOfChecked,
-    ...props
-  }) => {
-    
-    const [checked, setChecked] = useState(false)
-    const classes = cn('flex items-center', className)
-    console.log(checked)
-    return(
-        <div className={classes} {...props}>
-           {
-            <CheckboxUi.Root
-                className={variantsMap[variant!].root}
-                checked={typeOfChecked === 'checked' ? checked : undefined}
-                defaultChecked={typeOfChecked !== 'checked'}
-                id={id}
-                onCheckedChange={() => {
-                    if (typeOfChecked === 'checked') {
-                        setChecked(!checked);
-                    }
-                }}
-            >
-                <CheckboxUi.Indicator className={variantsMap[variant!].indicator}>
-                    <svg className="h-3 w-3" viewBox="0 0 33 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 10.909L4.364 6.54499L13.091 15.272L28.364 -0.00100708L32.728 4.36299L13.092 23.999L0 10.909Z" fill="currentColor" />
-                    </svg>
-                </CheckboxUi.Indicator>
-            </CheckboxUi.Root>
-           }
-            <label className={variantsMap[variant!].label} htmlFor={id}>
-                {children}
-            </label>
-        </div>
-    )
-  }
+const CheckboxRoot = forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({className, variant = 'checkbox', ...props}: CheckboxProps, forwardedRef) => (
+  <CheckboxPrimitive.Root
+    ref={forwardedRef}
+    className={cn(checkboxRoot({variant}), className)}
+    {...props}
+  />
+));
 
+const checkboxIndicator = cva(checkboxTheme.indicator, {
+  variants: {
+    variant: {
+      checkbox: checkboxTheme.indicator,
+      ring: checkBoxRingTheme.indicator,
+    }
+  },
+});
+
+export interface CheckboxIndicatorProps
+  extends VariantProps<typeof checkboxIndicator> {
+  className?: string;
+  props?: React.ComponentProps<typeof CheckboxPrimitive.Indicator>;
+}
+
+const CheckboxIndicator = forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Indicator>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Indicator>
+>(({className, variant = 'checkbox', ...props}: CheckboxIndicatorProps, forwardedRef) => (
+  <CheckboxPrimitive.Indicator
+    ref={forwardedRef}
+    className={cn(checkboxIndicator({variant}), className)}
+    {...props}
+  />
+));
+
+const checkboxLabel = cva(checkboxTheme.label, {
+  variants: {
+    variant: {
+      checkbox: checkboxTheme.label,
+      ring: checkBoxRingTheme.label,
+    }
+  },
+});
+
+export interface CheckboxLabelProps
+  extends VariantProps<typeof checkboxLabel> {
+  className?: string;
+  props?: React.ComponentProps<"label">;
+}
+
+const CheckboxLabel = forwardRef<
+  React.ElementRef<"label">,
+  React.ComponentPropsWithoutRef<"label">
+>(({className, variant = 'checkbox', ...props}: CheckboxLabelProps, forwardedRef) => (
+  <label
+    ref={forwardedRef}
+    className={cn(checkboxLabel({variant}), className)}
+    {...props}
+  />
+));
+
+export {
+    CheckboxRoot,
+    CheckboxIndicator,
+    CheckboxLabel,
+}
