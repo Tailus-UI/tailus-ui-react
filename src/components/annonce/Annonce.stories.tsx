433ccfe52af4a1c48ecd6d38c156e0d94a828f6f
Théo Balick <balickmethens@gmail.com>
2023-11-11 22:09:56 +0100

    Refactor Annonce component to use separate concern and message components

--- src/components/annonce/Annonce.stories.tsx
+++ src/components/annonce/Annonce.stories.tsx
@@ -1,27 +1,45 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Annonce } from "./Annonce";
+import { AnnonceConcern, AnnonceMessage, AnnonceRoot } from './Annonce.tsx';
 
+const AnnonceUI = ({
+  variant,
+  intent,
+  concern,
+  message,
+  href,
+} : {
+  variant: 'outlined' | 'soft',
+  intent: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'accent' | 'gray' | 'neutral',
+  concern: string,
+  message: string,
+  href: string
+}) => (
+  <AnnonceRoot href={href} variant={variant}>
+    <AnnonceConcern intent={intent}>{concern}</AnnonceConcern>
+    <AnnonceMessage>{message}</AnnonceMessage>
+  </AnnonceRoot>
+);
 
-const meta: Meta<typeof Annonce> = {
+const meta: Meta<typeof AnnonceUI> = {
   title: 'Annonce',
-  component: Annonce,
+  component: AnnonceUI,
   parameters: {
     layout: 'centered',
     docs: {
       description: {
-        component: 'A customizable Annonce component for React applications.',
+        component: 'A customizable Annonce component.',
       },
     },
   },
   tags: ['autodocs'],
   argTypes: {
-    variant: { 
-      control: 'select', 
-      options: ['soft', 'outlined'] 
+    variant: {
+      control: 'select',
+      options: ['soft', 'outlined']
     },
-    colorVariant: { 
-      control: 'select', 
-      options: ["primary", "secondary", "accent", "danger", "success", "warning", "info", "gray",] 
+    intent: {
+      control: 'select',
+      options: ["primary", "secondary", "accent", "danger", "success", "warning", "info", "gray",]
     },
     concern: {
       control: 'text',
@@ -39,24 +57,12 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Oulined: Story = {
-    args: {
-      variant: 'outlined',
-      colorVariant: "primary",
-      concern: "New",
-      message: "The all new version of Tailus is out !",
-      href: "https://tailus.io"
-    },
-};
-
-export const Soft: Story = {
+export const Annonce: Story = {
   args: {
-    variant: 'soft',
-    colorVariant: 'primary',
+    variant: 'outlined',
+    intent: "primary",
     concern: "New",
     message: "The all new version of Tailus is out !",
-      href: "https://tailus.io"
+    href: ""
   },
 };
-
-
