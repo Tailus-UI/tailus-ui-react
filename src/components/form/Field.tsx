57ed2e5c6cf00ae8ca4942363902b23c866074f6
Théo Balick <balickmethens@gmail.com>
2023-11-12 18:51:42 +0100

    Create a stylized alias of the Form.Field component

---
+++ src/components/form/Field.tsx
@@ -0,0 +1,36 @@
+import { Field } from "@radix-ui/react-form";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "../../lib/utils.ts";
+import React from "react";
+import {
+  outlinedForm as outlinedTheme,
+  softForm as softTheme
+} from "@tailus/themer-form";
+
+export type FormFieldVariantProps = VariantProps<typeof formFieldVariants>;
+const formFieldVariants = cva('', {
+  variants: {
+    variant: {
+      outlined: outlinedTheme.field,
+      soft: softTheme.field
+    },
+  }
+});
+
+export interface FormFieldProps extends FormFieldVariantProps {
+    className?: string;
+}
+
+const FormField = React.forwardRef<
+    React.ElementRef<typeof Field>,
+    React.ComponentPropsWithoutRef<typeof Field> & FormFieldProps
+>(({ className, variant = "outlined", name, ...props }, forwardedRef) => (
+    <Field
+        ref={forwardedRef}
+        name={name}
+        className={cn(formFieldVariants({ variant: variant }), className)}
+        {...props}
+    />
+));
+
+export default FormField;
