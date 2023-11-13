a1b7e080f59a5847bebdbdf731b7177d050cb070
Théo Balick <balickmethens@gmail.com>
2023-11-14 00:10:33 +0100

    Refactor FormInput and TextArea components

--- src/components/form/Textarea.tsx
+++ src/components/form/Textarea.tsx
@@ -1,54 +1,7 @@
 import React from 'react';
-import { softForm as softTHeme, outlinedForm as outlinedTheme } from "@tailus/themer-form";
-import { cva } from "class-variance-authority";
-import { cn } from '../../lib/utils.ts';
+import { cn, inputFormVariants } from '../../lib/utils.ts';
 
-const textareaVariants = cva("", {
-  variants: {
-    variant: {
-      soft: softTHeme.textarea.sm,
-      outlined: outlinedTheme.textarea.sm,
-    },
-    size: {
-      xs: outlinedTheme.textarea.xs,
-      sm: outlinedTheme.textarea.sm,
-      md: outlinedTheme.textarea.md,
-      lg: outlinedTheme.textarea.lg,
-      xl: outlinedTheme.textarea.xl,
-    }
-  },
-  compoundVariants: [
-    {
-      variant: "soft",
-      size: "xs",
-      class: softTHeme.textarea.xs
-    },
-    {
-      variant: "soft",
-      size: "sm",
-      class: softTHeme.textarea.sm
-    },
-    {
-      variant: "soft",
-      size: "md",
-      class: softTHeme.textarea.md
-    },
-    {
-      variant: "soft",
-      size: "lg",
-      class: softTHeme.textarea.lg
-    },
-    {
-      variant: "soft",
-      size: "xl",
-      class: softTHeme.textarea.xl
-    },
-  ],
-  defaultVariants: {
-    variant: "outlined",
-    size: "sm",
-  }
-});
+const textareaVariants = inputFormVariants("textarea");
 
 export interface TextAreaProps
   extends Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, 'size'> {
@@ -56,16 +9,13 @@ export interface TextAreaProps
     size?: "xs" | "sm" | "md" | "lg" | "xl";
   }
 
-export const FormTextArea: React.FC<TextAreaProps> = ({
-  className,
-  variant = "outlined",
-  size = "sm",
-  ...props
-}) => (
+  export const FormTextArea = React.forwardRef<
+  HTMLTextAreaElement, TextAreaProps>(({ className, variant, size, ...props }, forwardedRef) => (
   <textarea
-    className={cn(textareaVariants({ variant, size, className }))}
+    ref={forwardedRef as React.RefObject<HTMLTextAreaElement>}
+    className={cn(textareaVariants({ variant, size }), className)}
     {...props}
   />
-)
+));
 
 export default FormTextArea;
