4607a6c61aa4a998e0be0e840201e64fd08ef336
Théo Balick <balickmethens@gmail.com>
2023-11-14 00:11:16 +0100

    Add cn utility function to FormRoot component.

--- src/components/form/Root.tsx
+++ src/components/form/Root.tsx
@@ -1,14 +1,15 @@
 import {Root} from "@radix-ui/react-form";
 import {card as cardTheme} from "@tailus/themer-card";
 import React from "react";
+import { cn } from "../../lib/utils.ts";
 
 const FormRoot = React.forwardRef<
-  HTMLFormElement,
-  React.ComponentProps<typeof Root>>(
-    (props, forwardedRef) => (
+React.ElementRef<typeof Root>,
+React.ComponentPropsWithoutRef<typeof Root>
+>(({className, ...props}, forwardedRef) => (
     <Root
       ref={forwardedRef}
-      className={cardTheme}
+      className={cn(cardTheme, className)}
       {...props}
     />
 ));
