b593e784686c017988b517461e6d77b127fa8b98
Théo Balick <balickmethens@gmail.com>
2023-11-15 21:47:42 +0100

    move all Toggle's fragments in a single file

---
+++ src/components/toggle/index.ts
@@ -0,0 +1,11 @@
+import ToggleRoot from "./Root.tsx";
+import ToggleIcon from "./Icon.tsx";
+import ToggleIconBefore from "./IconBefore.tsx";
+import ToggleIconAfter from "./IconAfter.tsx";
+
+export {
+  ToggleRoot,
+  ToggleIconAfter,
+  ToggleIconBefore,
+  ToggleIcon
+}
