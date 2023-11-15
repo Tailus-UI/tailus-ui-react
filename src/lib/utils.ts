167cf08f12127d4fddca341de3140edca6136a11
Théo Balick <balickmethens@gmail.com>
2023-11-15 21:46:35 +0100

    move cloneElement function to utils directory

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import {clsx, ClassValue} from "clsx";
+import {twMerge} from "tailwind-merge";
+import React from "react";
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
+}
+
+export function cloneElement(element: React.ReactElement, classNames: string) {
+  return React.cloneElement(element, {
+    className: cn(element.props.className, classNames)
+  });
 }
