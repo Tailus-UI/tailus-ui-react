6d8df9355180d426c9ea437f7d820ddc0b740983
Théo Balick <balickmethens@gmail.com>
2023-11-16 10:45:41 +0100

    Update story in storybook

--- src/components/toggle/Toggle.stories.tsx
+++ src/components/toggle/Toggle.stories.tsx
@@ -9,14 +9,14 @@ type ToggleProps = {
 
 const ToggleUI = ({variant, intent, size}: ToggleProps) => (
   <ToggleRoot aria-label="Toggle italic" variant={variant} intent={intent} size={size}>
-    <ToggleIconBefore variant={variant} size={size}>
+    <ToggleIconBefore>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
            strokeWidth={1.5} stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round"
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
       </svg>
     </ToggleIconBefore>
-    <ToggleIconAfter variant={variant} intent={intent} size={size}>
+    <ToggleIconAfter>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
            fill="currentColor">
         <path
@@ -43,16 +43,19 @@ const meta: Meta<typeof ToggleUI> = {
       control: 'select',
       options: ['default', 'highlight'],
       defaultValue: 'default',
+      description: 'The variant of the toggle.'
     },
     intent: {
       control: 'select',
       options: ['primary', 'secondary', 'warning', 'danger', 'neutral', 'accent', 'success'],
-      defaultValue: 'accent'
+      defaultValue: 'accent',
+      description: 'The intent of the toggle.'
     },
     size: {
       control: 'select',
       options: ['xs', 'sm', 'md', 'lg', 'xl'],
-      defaultValue: 'md'
+      defaultValue: 'md',
+      description: 'The size of the toggle.',
     }
   },
 };
