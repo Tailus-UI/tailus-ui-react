63635d6c2921950da9a79d0aaa2b234474688ec7
Théo Balick <balickmethens@gmail.com>
2023-11-17 23:40:48 +0100

    [Update] Add comments

--- src/components/popover/Popover.tsx
+++ src/components/popover/Popover.tsx
@@ -3,11 +3,14 @@ import {popover} from "@tailus/themer-popover";
 import React from "react";
 import {cn} from "../../lib/utils.ts";
 
+// Define custom components based on the Popover components from @radix-ui/react-popover
 const PopoverRoot = Popover.Root;
 const PopoverTrigger = Popover.Trigger;
 const PopoverAnchor = Popover.Anchor;
 const PopoverPortal = Popover.Portal;
 
+// Creating a custom PopoverContent component with ref
+// This component is based on the Content component from @radix-ui/react-popover
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof Popover.Content>,
   React.ComponentPropsWithoutRef<typeof Popover.Content>
@@ -19,6 +22,10 @@ const PopoverContent = React.forwardRef<
   />
 ));
 
+// Creating a custom PopoverTitle component with ref
+// This component is a div that takes all props of a standard div
+// and passes them to the actual div. Additionally, it merges passed class names
+// with those of the popover theme.
 const PopoverTitle = React.forwardRef<
   React.ElementRef<"div">,
   React.ComponentPropsWithoutRef<"div">
@@ -30,6 +37,10 @@ const PopoverTitle = React.forwardRef<
   />
 ));
 
+// Creating a custom PopoverDescription component with ref
+// This component is a paragraph that takes all props of a standard paragraph
+// and passes them to the actual paragraph. Additionally, it merges passed class names
+// with those of the popover theme.
 const PopoverDescription = React.forwardRef<
   React.ElementRef<"p">,
   React.ComponentPropsWithoutRef<"p">
@@ -41,36 +52,41 @@ const PopoverDescription = React.forwardRef<
   />
 ));
 
+// Creating a custom PopoverClose component with ref
+// This component is based on the Close component from @radix-ui/react-popover
 const PopoverClose = React.forwardRef<
-    React.ElementRef<typeof Popover.Close>,
-    React.ComponentPropsWithoutRef<typeof Popover.Close>
+  React.ElementRef<typeof Popover.Close>,
+  React.ComponentPropsWithoutRef<typeof Popover.Close>
 >(({className, ...props}, forwardedRef) => (
-    <Popover.Close
-        {...props}
-        ref={forwardedRef}
-        className={cn(popover.close, className)}
-    />
+  <Popover.Close
+    {...props}
+    ref={forwardedRef}
+    className={cn(popover.close, className)}
+  />
 ));
 
+// Creating a custom PopoverArrow component with ref
+// This component is based on the Arrow component from @radix-ui/react-popover
 const PopoverArrow = React.forwardRef<
-    React.ElementRef<typeof Popover.Arrow>,
-    React.ComponentPropsWithoutRef<typeof Popover.Arrow>
+  React.ElementRef<typeof Popover.Arrow>,
+  React.ComponentPropsWithoutRef<typeof Popover.Arrow>
 >(({className, ...props}, forwardedRef) => (
-    <Popover.Arrow
-        {...props}
-        ref={forwardedRef}
-        className={cn(popover.arrow, className)}
-    />
+  <Popover.Arrow
+    {...props}
+    ref={forwardedRef}
+    className={cn(popover.arrow, className)}
+  />
 ));
 
+// Exporting all fragments of the popover component
 export {
-    PopoverRoot,
-    PopoverTrigger,
-    PopoverAnchor,
-    PopoverPortal,
-    PopoverContent,
-    PopoverTitle,
-    PopoverDescription,
-    PopoverClose,
-    PopoverArrow
+  PopoverRoot,
+  PopoverTrigger,
+  PopoverAnchor,
+  PopoverPortal,
+  PopoverContent,
+  PopoverTitle,
+  PopoverDescription,
+  PopoverClose,
+  PopoverArrow
 };
