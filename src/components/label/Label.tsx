ec7c8f49d75bad47b6d95653243164ee609b895b
Théo Balick <balickmethens@gmail.com>
2023-11-17 22:04:20 +0100

    [Build] the Root part of the Label component

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,13 +1,29 @@
 import * as Label from "@radix-ui/react-label";
-import { outlinedForm as form } from "@tailus/themer-form";
+import {outlinedForm as outlinedTheme, softForm, softForm as softTheme} from "@tailus/themer-form";
+import React from "react";
+import {cn} from "../../lib/utils.ts";
+
+interface LabelRootProps
+  extends React.ComponentPropsWithoutRef<typeof Label.Root> {
+  variant?: "outlined" | "soft";
+}
+
+export const LabelRoot = React.forwardRef<
+  React.ElementRef<typeof Label.Root>, LabelRootProps
+>(({className, variant = "outlined", ...props}, forwardedRef) => {
+  const theme = variant === "outlined" ? outlinedTheme : softTheme;
+  return (
+    <Label.Root ref={forwardedRef} className={cn(theme.label.sm, className)} {...props} />
+  )
+});
 
 const LabelUI = () => (
-    <div className="space-y-2">
-        <Label.Root className={form.label.sm} htmlFor="firstName">
-            First name
-        </Label.Root>
-        <input className={form.input.md} name="firstName" id="firstName" defaultValue="Méschac Irung" type="text" />
-    </div>
+  <div className="space-y-2">
+    <LabelRoot htmlFor="firstName">
+      First name
+    </LabelRoot>
+    <input className={softForm.input.md} name="firstName" id="firstName" defaultValue="Méschac Irung" type="text"/>
+  </div>
 );
 
 export default LabelUI;
