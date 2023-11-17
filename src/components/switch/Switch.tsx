eacc09b37baf0aa6f8ec4e417cc1de34272aa155
Théo Balick <balickmethens@gmail.com>
2023-11-17 21:17:55 +0100

    [Build] Creating custom Switch fragments

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -1,17 +1,39 @@
 import * as Switch from "@radix-ui/react-switch";
-import { switchTheme } from "@tailus/themer-switch";
+import {switchTheme} from "@tailus/themer-switch";
+import {cn} from "../../lib/utils.ts";
+import React from "react";
 
-const SwitchUI = () => (
-    <form>
-        <div className={switchTheme.parentElement}>
-            <Switch.Root className={switchTheme.root} id="airplane-mode">
-                <Switch.Thumb className={switchTheme.thumb} />
-            </Switch.Root>
-            <label className={switchTheme.label} htmlFor="airplane-mode">
-                Airplane mode
-            </label>
-        </div>
-    </form>
-);
+const SwitchContainer = React.forwardRef<
+  React.ElementRef<"div">,
+  React.ComponentPropsWithoutRef<"div">
+>(({className, ...props}, forwardedRef) => (
+  <div className={cn(switchTheme.parentElement, className)} {...props} ref={forwardedRef}/>
+));
 
-export default SwitchUI;
+const SwitchRoot = React.forwardRef<
+  React.ElementRef<typeof Switch.Root>,
+  React.ComponentPropsWithoutRef<typeof Switch.Root>
+>(({className, ...props}, forwardedRef) => (
+  <Switch.Root className={cn(switchTheme.root, className)} {...props} ref={forwardedRef}/>
+));
+
+const SwitchThumb = React.forwardRef<
+  React.ElementRef<typeof Switch.Thumb>,
+  React.ComponentPropsWithoutRef<typeof Switch.Thumb>
+>(({className, ...props}, forwardedRef) => (
+  <Switch.Thumb className={cn(switchTheme.thumb, className)} {...props} ref={forwardedRef}/>
+));
+
+const SwitchLabel = React.forwardRef<
+  React.ElementRef<"label">,
+  React.ComponentPropsWithoutRef<"label">
+>(({className, ...props}, forwardedRef) => (
+  <label className={cn(switchTheme.label, className)} {...props} ref={forwardedRef}/>
+));
+
+export {
+  SwitchContainer,
+  SwitchRoot,
+  SwitchThumb,
+  SwitchLabel,
+};
