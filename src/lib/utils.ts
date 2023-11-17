e4fc8c7d57e8b2b19a9c26b3aa47bac4655d467b
Théo Balick <balickmethens@gmail.com>
2023-11-17 15:26:16 +0100

    Comment functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,25 @@ import {clsx, ClassValue} from "clsx";
 import {twMerge} from "tailwind-merge";
 import React from "react";
 
+/**
+ * Tailwind CSS classnames merge.
+ * The function resolves conflicting Tailwind CSS classnames and merges them into one
+ * @param { string | string[] } inputs - Tailwind CSS classnames
+ * @returns { string } - Merged Tailwind CSS classnames
+ * @example cn("text-red-500", "text-center", "text-cyan-500") // => "text-center text-cyan-500"
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Clone React element.
+ * The function clones React element and adds Tailwind CSS classnames to the cloned element
+ * @param element the React element to clone
+ * @param classNames Tailwind CSS classnames
+ * @returns { React.ReactElement } - Cloned React element
+ * @example cloneElement(<div />, "text-center text-cyan-500") // => <div className="text-center text-cyan-500" />
+ */
 export function cloneElement(element: React.ReactElement, classNames: string) {
   return React.cloneElement(element, {
     className: cn(element.props.className, classNames)
