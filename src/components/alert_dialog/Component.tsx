6a0cf8f61f224b5fc545567462d6358a8cf5cfb4
Théo Balick <balickmethens@gmail.com>
2023-11-18 11:00:24 +0100

    [Build] Create a custom Overlay component

--- src/components/alert_dialog/Component.tsx
+++ src/components/alert_dialog/Component.tsx
@@ -3,13 +3,34 @@ import {
   centredAlertDialog as centredTheme,
 } from "@tailus/themer-alert-dialog"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
+import {cn} from "../../lib/utils.ts";
+import React from "react";
 
 const AlertDialogRoot = AlertDialogPrimitive.Root;
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 const AlertDialogPortal = AlertDialogPrimitive.Portal;
 
+type AlertDialogProps = {
+    variant?: "centred" | "default"
+}
+
+const AlertDialogOverlay = React.forwardRef<
+    React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
+    React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay> & AlertDialogProps
+>(({className, variant = 'default', ...props}, forwardedRef) => {
+  const theme = variant === "centred" ? centredTheme : defaultTheme;
+  return (
+    <AlertDialogPrimitive.Overlay
+      {...props}
+      ref={forwardedRef}
+      className={cn(theme.overlay, className)}
+    />
+  )
+})
+
 export {
   AlertDialogRoot,
   AlertDialogTrigger,
   AlertDialogPortal,
+  AlertDialogOverlay
 }
