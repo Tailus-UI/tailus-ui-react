8c193be8d13cf4323acee7e3bcede87f5877c234
Théo Balick <balickmethens@gmail.com>
2023-11-18 08:53:01 +0100

    [Build] Create a custom Root component

---
+++ src/components/alert_dialog/Component.tsx
@@ -0,0 +1,11 @@
+import {
+  alertDialog as defaultTheme,
+  centredAlertDialog as centredTheme,
+} from "@tailus/themer-alert-dialog"
+import * as AlertDialog from "@radix-ui/react-alert-dialog";
+
+const AlertDialogRoot = AlertDialog.Root;
+
+export {
+  AlertDialogRoot,
+}
