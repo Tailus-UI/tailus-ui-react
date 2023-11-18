7ca756e5c942979257d96f705c67a7607dedada0
Théo Balick <balickmethens@gmail.com>
2023-11-18 10:20:49 +0100

    [Build] Create a custom Trigger & Portal components

--- src/components/alert_dialog/Component.tsx
+++ src/components/alert_dialog/Component.tsx
@@ -2,10 +2,14 @@ import {
   alertDialog as defaultTheme,
   centredAlertDialog as centredTheme,
 } from "@tailus/themer-alert-dialog"
-import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 
-const AlertDialogRoot = AlertDialog.Root;
+const AlertDialogRoot = AlertDialogPrimitive.Root;
+const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
+const AlertDialogPortal = AlertDialogPrimitive.Portal;
 
 export {
   AlertDialogRoot,
+  AlertDialogTrigger,
+  AlertDialogPortal,
 }
