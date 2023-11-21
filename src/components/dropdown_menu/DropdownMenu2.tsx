0869082b1c845145fb44eacce6d84183367b93e0
Théo Balick <balickmethens@gmail.com>
2023-11-21 14:13:28 +0100

    Create a DropdownRoot component based on Radix's DropDownRoot component

---
+++ src/components/dropdown_menu/DropdownMenu2.tsx
@@ -0,0 +1,14 @@
+//import {dropdownMenu as defaultTheme, softDropdownMenu as softTheme} from "@tailus/themer-dropdown-menu";
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+
+const DropdownMenuRoot = DropdownMenuPrimitive.Root;
+
+const DropdownMenu = {
+  Root: DropdownMenuRoot,
+};
+
+export default DropdownMenu;
+
+export {
+  DropdownMenuRoot,
+}
