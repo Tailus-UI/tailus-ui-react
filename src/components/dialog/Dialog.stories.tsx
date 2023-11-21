b9af393a7dfce9ac7240238ac1aa8e60c53e9e46
Théo Balick <balickmethens@gmail.com>
2023-11-21 05:35:51 +0100

    Using the custom Toggle component in the Example2 component in the Dialog.stories.tsx file

--- src/components/dialog/Dialog.stories.tsx
+++ src/components/dialog/Dialog.stories.tsx
@@ -1,11 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import RadioGroupUI from "../radio_group/RadioGroup";
-import {button as buttonTheme, ghostIconButton} from "@tailus/themer-button"
+import {button as buttonTheme, ghostButton, ghostIconButton} from "@tailus/themer-button"
 import {dialog} from "@tailus/themer-dialog"
 import {Cross2Icon} from "@radix-ui/react-icons"
 import DialogExample2 from "./Example2.tsx";
 import DialogExample3 from "./Example3.tsx";
 import ToggleGroup from "../toggle_group/ToggleGroup.tsx";
+import Toggle from "../toggle/Toggle.tsx";
 
 const DialogUI = () => (
   <Dialog.Root>
@@ -76,6 +77,45 @@ const DialogUI = () => (
             <Cross2Icon className={ghostIconButton.icon.md} aria-hidden/>
           </button>
         </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
+
+const DialogExample2 = () => (
+  <Dialog.Root>
+    <Dialog.Trigger>
+      <Toggle.Root value="bookmark" aria-label="Bookmark">
+        <Toggle.IconBefore>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+          </svg>
+        </Toggle.IconBefore>
+        <Toggle.IconAfter>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z" clipRule="evenodd" />
+          </svg>
+        </Toggle.IconAfter>
+      </Toggle.Root>
+    </Dialog.Trigger>
+    <Dialog.Portal>
+      <Dialog.Overlay className={dialog.overlay} />
+      <Dialog.Content className={dialog.content}>
+        <Dialog.Title className={dialog.title}>Sign in to bookmark</Dialog.Title>
+        <Dialog.Description className={dialog.description}>You need to be signed in to save your frequently used components.</Dialog.Description>
+
+        <div className={dialog.actions}>
+          <Dialog.Close>
+            <button className={ghostButton.primary.md}>
+              <span>Cancel</span>
+            </button>
+          </Dialog.Close>
+          <Dialog.Close>
+            <button className={buttonTheme.primary.md}>
+              <span>Sign in</span>
+            </button>
+          </Dialog.Close>
+        </div>
       </Dialog.Content>
     </Dialog.Portal>
   </Dialog.Root>
