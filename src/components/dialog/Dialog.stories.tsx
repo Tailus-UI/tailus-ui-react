440ed533363367f802e4a95c2dd4a38062456446
Théo Balick <balickmethens@gmail.com>
2023-11-21 09:53:19 +0100

    Update component rendering in Storybook

--- src/components/dialog/Dialog.stories.tsx
+++ src/components/dialog/Dialog.stories.tsx
@@ -1,15 +1,29 @@
-import Dialog from "./Example.tsx";
+import {Meta} from "@storybook/react";
+import DialogUI from "./Example";
 
 const Template = () => {
   return (
-    <Dialog.Example1/>
+    <DialogUI.Example1/>
   );
+};
+
+const meta: Meta<typeof Template> = {
+  title: "Dialog",
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "The dialog component theme is a collection of Tailwindcss utilities that can be used to " +
+          "create dialogs with a single variant. Dialogs are modal windows that appear in front of app content " +
+          "to provide critical information or ask for a decision.",
+      }
+    }
+  },
+  tags: ['autodocs'],
 }
 
-export default {
-  component: Template,
-}
+export default meta;
 
-export const Example1 = () => Dialog.Example1;
-export const Example2 = () => Dialog.Example2;
-export const Example3 = () => Dialog.Example3;
+export const Example1 = () => <DialogUI.Example1/>;
+export const Example2 = () => <DialogUI.Example2/>;
+export const Example3 = () => <DialogUI.Example3/>;
