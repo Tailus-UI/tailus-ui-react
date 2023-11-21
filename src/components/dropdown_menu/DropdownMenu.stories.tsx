5a1c5a73b962f76b0cc664b32e5bc483a6a5a68b
Théo Balick <balickmethens@gmail.com>
2023-11-21 14:14:12 +0100

    Update Stories in storybook

--- src/components/dropdown_menu/DropdownMenu.stories.tsx
+++ src/components/dropdown_menu/DropdownMenu.stories.tsx
@@ -1,4 +1,4 @@
-import DropdownMenuUI from "./DropdownMenu";
+/*import DropdownMenuUI from "./DropdownMenu";
 import SoftDropdownMenuUI from "./SoftDropdownMenu";
 import DropdownMenuSubMenu from "./SolidWithSubMenu";
 import SoftDropdownSubMenu from "./SoftWithSubMenu";
@@ -10,4 +10,56 @@ export default {
 export const DropdownMenu = () => <DropdownMenuUI />;
 export const Soft = () => <SoftDropdownMenuUI />;
 export const WithSubMenu = () => <DropdownMenuSubMenu />;
-export const SoftWithSubMenu = () => <SoftDropdownSubMenu />;
+export const SoftWithSubMenu = () => <SoftDropdownSubMenu />;*/
+
+import DropdownMenu from "./DropdownMenu2.tsx";
+import {Meta, StoryObj} from "@storybook/react";
+
+type DropdownMenuUIProps = {
+    variant?: "default" | "soft",
+}
+
+const DropdownMenuUI = (args: DropdownMenuUIProps) => {
+    return (
+      <DropdownMenu.Root>
+
+      </DropdownMenu.Root>
+    );
+}
+
+const meta: Meta<typeof DropdownMenuUI> = {
+    title: "DropdownMenu",
+    component: DropdownMenuUI,
+    parameters: {
+        layout: "centered",
+        docs: {
+            description: {
+                component: "The dropdown menu component theme is a collection of Tailwind utilities that can be " +
+                  "used to create dropdown menus with different variants and colors. The default variant is " +
+                  "solid, but there is also a soft variant. Each variant has a number of properties that can " +
+                  "be customized, including the shadow, border colors, background opacity, backdrop blur, " +
+                  "radius, and separator colors."
+            }
+        }
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        variant: {
+            name: "Variant",
+            control: "radio",
+            description: "The variant of the dropdown menu content.",
+            defaultValue: "default",
+            options: ["default", "soft"],
+        },
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Template: Story = {
+    args: {
+        variant: "default",
+    }
+}
