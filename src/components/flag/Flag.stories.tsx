94d324ebf90a6af4c63a4dc37728af17566ec389
Théo Balick <balickmethens@gmail.com>
2023-11-22 21:08:35 +0100

    Update Stories in storybook

--- src/components/flag/Flag.stories.tsx
+++ src/components/flag/Flag.stories.tsx
@@ -1,9 +1,71 @@
-import FlagUI from "./Flag";
-import FlagWithMessage from "./WithMessage";
+import {Meta} from "@storybook/react";
+import { flag as flagTheme, flagWithMessage } from "@tailus/themer-flag"
 
-export default {
-    component: FlagUI,
+export const Example = () => (
+  <div className={flagTheme.root + " min-w-[24rem]"} aria-label="Error flag" aria-describedby="error-flag-description" role="alert" >
+      <div className={flagTheme.icon.parent}>
+          <svg className={flagTheme.icon.danger} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clipRule="evenodd" />
+          </svg>
+      </div>
+      <p className={flagTheme.title.danger} id="error-flag-description">
+          Update failed !{" "}
+          <a href="#" className={flagTheme.link}>
+              Learn more
+          </a>{" "}
+      </p>
+      <button className={flagTheme.close.button}>
+          <span className="sr-only">Dismiss alert</span>
+          <svg className={flagTheme.close.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+          </svg>
+      </button>
+  </div>
+)
+
+export const ExampleWithMessage = () => (
+  <div className={flagWithMessage.root} aria-label="Error flag" aria-describedby="error-flag-description-2" role="alert" >
+      <div className={flagWithMessage.icon.parent}>
+          <svg className={flagWithMessage.icon.info} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+          </svg>
+      </div>
+      <div className={flagWithMessage.content}>
+          <h6 className={flagWithMessage.title.info}>New update available</h6>
+          <p id="error-flag-description-2" className={flagWithMessage.message}>
+              Radix UI support, Modern UI, Customization...
+          </p>
+          <div className={flagTheme.actions}>
+              <a href="#" className={flagWithMessage.link}>
+                  Learn more
+              </a>
+              <a href="#" className={flagWithMessage.link}>
+                  Upgrade now
+              </a>
+          </div>
+      </div>
+      <button className={flagWithMessage.close.button}>
+          <span className="sr-only">Dismiss alert</span>
+          <svg className={flagWithMessage.close.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+          </svg>
+      </button>
+  </div>
+)
+
+const meta: Meta = {
+    title: "Flag",
+    parameters: {
+        layout: "centered",
+        docs: {
+            description: {
+                component: "The @tailus/themer-flag package provides a theme for the flag component. The flag " +
+                  "component is a versatile component that can be used to display alerts, notifications, and " +
+                  "other types of messages.",
+            }
+        }
+    },
+    tags: ['autodocs'],
 }
 
-export const Flag = () => <FlagUI />;
-export const WithMessage = () => <FlagWithMessage />;
+export default meta;
