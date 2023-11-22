4c54606060e6824c6a02dbaf6455c83945744b91
Théo Balick <balickmethens@gmail.com>
2023-11-22 14:46:52 +0100

    Refactor EmptyStateRoot component & add possibility of adding extra classNames

--- src/components/empty_state/EmptyState.tsx
+++ src/components/empty_state/EmptyState.tsx
@@ -1,12 +1,17 @@
 import {emptyState} from "@tailus/themer-empty-state"
 import React from "react";
+import {cn} from "../../lib/utils.ts";
 
 type Intent = "primary" | "secondary" | "accent" | "success" | "warning" | "danger" | "info" | "gray";
 
 const EmptyStateRoot = React.forwardRef<
   HTMLDivElement, React.HTMLAttributes<HTMLDivElement>
->((props, ref) => (
-  <div ref={ref} className={emptyState.root} {...props} />
+>(({className, ...props}, ref) => (
+  <div
+    ref={ref}
+    className={cn(emptyState.root, className)}
+    {...props}
+  />
 ));
 
 const EmptyState = {
