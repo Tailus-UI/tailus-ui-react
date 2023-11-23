0105d2dd1e4dda25fd9f18bb65a709890d0893ed
Théo Balick <balickmethens@gmail.com>
2023-11-23 22:12:21 +0100

    [RadioGroup] Update Story in storybook

--- src/components/radio_group/RadioGroup.stories.tsx
+++ src/components/radio_group/RadioGroup.stories.tsx
@@ -1,9 +1,71 @@
-import RadioGroupUI from "./RadioGroup";
-import RadioGroupShadowVariant from "./ShadowRadio";
+import RadioGroup from "./RadioGroup";
+import {Meta, StoryObj} from "@storybook/react";
 
-export default {
-    component: RadioGroupUI,
+interface RadioGroupProps {
+  withShadow?: boolean;
 }
 
-export const RadioGroup = () => <RadioGroupUI />;
-export const Shadow = () => <RadioGroupShadowVariant />
+const RadioGroupUI = (ars: RadioGroupProps) => (
+  <form>
+    <RadioGroup.Root className="flex flex-col gap-2.5" defaultValue="problem" aria-label="View density" withShadow={ars.withShadow}>
+      <div className="flex items-center">
+        <RadioGroup.Item value="understanding" id="rr1">
+          <RadioGroup.Indicator/>
+        </RadioGroup.Item>
+        <RadioGroup.Label htmlFor="rr1">
+          Easy to understand
+        </RadioGroup.Label>
+      </div>
+      <div className="flex items-center">
+        <RadioGroup.Item value="problem" id="rr2">
+          <RadioGroup.Indicator/>
+        </RadioGroup.Item>
+        <RadioGroup.Label htmlFor="rr2">
+          Solved my problem
+        </RadioGroup.Label>
+      </div>
+      <div className="flex items-center">
+        <RadioGroup.Item value="other" id="rr3">
+          <RadioGroup.Indicator/>
+        </RadioGroup.Item>
+        <RadioGroup.Label htmlFor="rr3">
+          Other
+        </RadioGroup.Label>
+      </div>
+    </RadioGroup.Root>
+  </form>
+);
+
+const meta: Meta<typeof RadioGroupUI> = {
+  title: 'RadioGroup',
+  component: RadioGroupUI,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'The radio group component theme is a collection of Tailwind CSS utilities that can be' +
+          ' used to create radio groups with different variants. Radio groups are used to allow users' +
+          ' to select a single option from a list.',
+      },
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    withShadow: {
+      name: 'withShadow',
+      description: 'Whether to use the shadow variant',
+      type: {name: 'boolean', required: false},
+      control: 'boolean'
+    },
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Toggle: Story = {
+  args: {
+    withShadow: false,
+  },
+};
