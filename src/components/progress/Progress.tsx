3dc9098ff3435a7321c3570ba946d930e515f8f9
Théo Balick <balickmethens@gmail.com>
2023-11-23 14:54:33 +0100

    [Progress] Build the Root part

--- src/components/progress/Progress.tsx
+++ src/components/progress/Progress.tsx
@@ -1,28 +1,30 @@
 import React from "react";
-import * as Progress from "@radix-ui/react-progress";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 import {progress as theme} from "@tailus/themer-progress";
+import {cn} from "../../lib/utils.ts";
 
-const ProgressUI = () => {
-    const [progress, setProgress] = React.useState(0);
+const ProgressRoot = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>((props, forwardedRef) => (
+  <ProgressPrimitive.Root
+    {...props}
+    ref={forwardedRef}
+    className={cn(theme.root, props.className)}
+    style={{
+      // Fix overflow clipping in Safari
+      // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
+      transform: "translateZ(0)",
+    }}
+  />
+));
 
-    React.useEffect(() => {
-        const timer = setTimeout(() => setProgress(32), 500);
-        return () => clearTimeout(timer);
-    }, []);
+const Progress = {
+  Root: ProgressRoot,
+}
 
-    return (
-        <Progress.Root
-            className={theme.root + " w-64"}
-            style={{
-                // Fix overflow clipping in Safari
-                // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
-                transform: "translateZ(0)",
-            }}
-            value={progress}
-        >
-            <Progress.Indicator className={theme.indicator.primary} style={{ transform: `translateX(-${100 - progress}%)` }} />
-        </Progress.Root>
-    );
-};
+export default Progress;
 
-export default ProgressUI;
+export {
+  ProgressPrimitive,
+}
