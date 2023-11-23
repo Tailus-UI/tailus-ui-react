292343d7befaea929cc1c70d9cf441e6ef7721f0
Théo Balick <balickmethens@gmail.com>
2023-11-23 13:01:06 +0100

    Define the context, the value type of the component variant and the CloseButton part

--- src/components/flag/Flag.tsx
+++ src/components/flag/Flag.tsx
@@ -1,30 +1,49 @@
-import {flagWithMessage as theme} from "@tailus/themer-flag"
+import {flag as defaultTheme, flagWithMessage as theme} from "@tailus/themer-flag"
 import {cloneElement, cn} from "../../lib/utils.ts";
 import React from "react";
+import {Button} from "../button/Button.tsx";
 
 type Intent = "success" | "warning" | "danger" | "info" | "gray";
+type Variant = "default" | "withMessage";
+
+const defaultVariant = "default";
+const VariantContext = React.createContext<Variant>(defaultVariant);
+
+interface FlagProps {
+  withMessage?: boolean,
+}
 
 const FlagRoot = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({className, ...props}, ref) => (
-  <div
-    ref={ref}
-    className={cn(theme.root, className)}
-    {...props}
-  />
-));
+  React.HTMLAttributes<HTMLDivElement> & FlagProps
+>(({className, withMessage, ...props}, ref) => {
+  const variant = withMessage ? "withMessage" : "default";
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
+  return (
+    <VariantContext.Provider value={variant}>
+      <div
+        ref={ref}
+        className={cn(chosenTheme.root, className)}
+        {...props}
+      />
+    </VariantContext.Provider>
+  )
+});
 
 const FlagIconContainer = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({className, ...props}, ref) => (
-  <div
-    ref={ref}
-    className={cn(theme.icon.parent, className)}
-    {...props}
-  />
-));
+>(({className, ...props}, ref) => {
+  const variant = React.useContext(VariantContext);
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
+  return (
+    <div
+      ref={ref}
+      className={cn(chosenTheme.icon.parent, className)}
+      {...props}
+    />
+  )
+});
 
 interface FlagIconProps {
   className?: string,
@@ -33,12 +52,13 @@ interface FlagIconProps {
 }
 
 const FlagIcon = ({className, intent = "info", children}: FlagIconProps) => {
+  const variant = React.useContext(VariantContext);
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
   return cloneElement(
     children as React.ReactElement,
-    cn(theme.icon[intent], className)
+    cn(chosenTheme.icon[intent], className)
   );
 };
-
 
 interface FlagTitleProps {
   className?: string,
@@ -48,13 +68,17 @@ interface FlagTitleProps {
 const FlagTitle = React.forwardRef<
   HTMLHeadingElement,
   React.HTMLAttributes<HTMLHeadingElement> & FlagTitleProps
->(({className, intent = "info", ...props}, ref) => (
-  <h6
-    ref={ref}
-    className={cn(theme.title[intent], className)}
-    {...props}
-  />
-));
+>(({className, intent = "info", ...props}, ref) => {
+  const variant = React.useContext(VariantContext);
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
+  return (
+    <h6
+      ref={ref}
+      className={cn(chosenTheme.title[intent], className)}
+      {...props}
+    />
+  )
+});
 
 const FlagContent = React.forwardRef<
   HTMLDivElement,
@@ -70,13 +94,17 @@ const FlagContent = React.forwardRef<
 const FlagLink = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({className, ...props}, ref) => (
-  <a
-    ref={ref}
-    className={cn(theme.link, className)}
-    {...props}
-  />
-));
+>(({className, ...props}, ref) => {
+  const variant = React.useContext(VariantContext);
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
+  return (
+    <a
+      ref={ref}
+      className={cn(chosenTheme.link, className)}
+      {...props}
+    />
+  )
+});
 
 const FLagMessage = React.forwardRef<
   HTMLParagraphElement,
@@ -92,13 +120,41 @@ const FLagMessage = React.forwardRef<
 const FlagActions = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({className, ...props}, ref) => (
-  <div
-    ref={ref}
-    className={cn(theme.actions, className)}
-    {...props}
-  />
-));
+>(({className, ...props}, ref) => {
+  const variant = React.useContext(VariantContext);
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
+  return (
+    <div
+      ref={ref}
+      className={cn(chosenTheme.actions, className)}
+      {...props}
+    />
+  )
+});
+
+interface FlagCloseButtonProps {
+  children?: React.ReactNode,
+}
+
+const FlagCloseButton = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<typeof Button> & FlagCloseButtonProps
+>(({className, children, ...props}, ref) => {
+  const variant = React.useContext(VariantContext);
+  const chosenTheme = variant === "default" ? defaultTheme : theme;
+  return (
+    <button
+      ref={ref}
+      className={cn(chosenTheme.close.button, className)}
+      {...props}
+    >
+      {cloneElement(
+        children as React.ReactElement,
+        chosenTheme.close.icon
+      )}
+    </button>
+  )
+});
 
 const Flag = {
   Root: FlagRoot,
@@ -109,6 +165,7 @@ const Flag = {
   Link: FlagLink,
   Message: FLagMessage,
   Actions: FlagActions,
+  CloseButton: FlagCloseButton,
 };
 
 export default Flag;
@@ -122,4 +179,5 @@ export {
   FlagLink,
   FLagMessage,
   FlagActions,
+  FlagCloseButton,
 }
