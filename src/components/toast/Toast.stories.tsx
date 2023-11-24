73732a7137e603b6e545abde4bc4537ce35418ed
Théo Balick <balickmethens@gmail.com>
2023-11-24 16:03:19 +0100

    [Toast] Building the Provider and Root parts

--- src/components/toast/Toast.stories.tsx
+++ src/components/toast/Toast.stories.tsx
@@ -1,7 +1,94 @@
-import ToastUI from "./Toast";
+import {Meta, StoryObj} from "@storybook/react";
+import * as React from "react";
+import * as ToastPrimitive from "@radix-ui/react-toast";
+import {toast as theme} from "@tailus/themer-toast";
+import {ghostButton, ghostIconButton, button} from "@tailus/themer-button";
+import {Cross2Icon} from "@radix-ui/react-icons";
+import Toast from "./Toast";
 
-export default {
-    component: ToastUI,
+const ToastUI = () => {
+  const [open, setOpen] = React.useState(false);
+  const eventDateRef = React.useRef(new Date());
+  const timerRef = React.useRef(0);
+
+  React.useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
+
+  return (
+    <Toast.Provider swipeDirection="center">
+      <button
+        className={button.primary.lg}
+        onClick={() => {
+          setOpen(false);
+          window.clearTimeout(timerRef.current);
+          timerRef.current = window.setTimeout(() => {
+            eventDateRef.current = oneWeekAway();
+            setOpen(true);
+          }, 100);
+        }}
+      >
+        <span>Add to calendar</span>
+      </button>
+
+      <Toast.Root open={open} onOpenChange={setOpen}>
+        <div className={theme.header}>
+          <ToastPrimitive.Title className={theme.title}>Scheduled: Catch up</ToastPrimitive.Title>
+          <div className={theme.actions}>
+            <ToastPrimitive.Action asChild altText="Goto schedule to undo">
+              <button className={ghostButton.primary.sm}>
+                <span>Undo</span>
+              </button>
+            </ToastPrimitive.Action>
+            <ToastPrimitive.Close>
+              <button className={ghostIconButton.gray.sm}>
+                <span className="sr-only">Dismiss toast</span>
+                <Cross2Icon className={ghostIconButton.icon.md} aria-hidden/>
+              </button>
+            </ToastPrimitive.Close>
+          </div>
+        </div>
+        <ToastPrimitive.Description asChild>
+          <time className={theme.description} dateTime={eventDateRef.current.toISOString()}>
+            {prettyDate(eventDateRef.current)}
+          </time>
+        </ToastPrimitive.Description>
+      </Toast.Root>
+      <ToastPrimitive.Viewport className={theme.viewport}/>
+    </Toast.Provider>
+  );
 };
 
-export const Toast = () => <ToastUI />;
+function oneWeekAway() {
+  const now = new Date();
+  const inOneWeek = now.setDate(now.getDate() + 7);
+  return new Date(inOneWeek);
+}
+
+function prettyDate(date: Date) {
+  return new Intl.DateTimeFormat("en-US", {dateStyle: "full", timeStyle: "short"}).format(date);
+}
+
+const meta: Meta<typeof ToastUI> = {
+  title: 'Toast',
+  component: ToastUI,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'The toast component theme is a collection of Tailwindcss utilities that can be used ' +
+          'to create toasts with a single variant. Toasts are small, unobtrusive notifications that appear' +
+          ' on the screen to inform users of an event or to provide feedback.',
+      },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  name: 'Toast',
+};
--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,70 +1,31 @@
 import * as React from "react";
-import * as Toast from "@radix-ui/react-toast";
-import { toast as theme } from "@tailus/themer-toast";
-import { ghostButton, ghostIconButton, button } from "@tailus/themer-button";
-import {Cross2Icon} from "@radix-ui/react-icons";
+import * as ToastPrimitive from "@radix-ui/react-toast";
+import {toast as theme} from "@tailus/themer-toast";
+import {cn} from "../../lib/utils.ts";
 
-const ToastUI = () => {
-    const [open, setOpen] = React.useState(false);
-    const eventDateRef = React.useRef(new Date());
-    const timerRef = React.useRef(0);
+const ToastProvider = ToastPrimitive.Provider;
 
-    React.useEffect(() => {
-        return () => clearTimeout(timerRef.current);
-    }, []);
+const ToastRoot = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitive.Root>,
+  React.ComponentProps<typeof ToastPrimitive.Root>
+>((props, forwardedRef) => {
+  return (
+    <ToastPrimitive.Root
+      ref={forwardedRef}
+      className={cn(theme.root, props.className)}
+      {...props}
+    />
+  );
+});
 
-    return (
-        <Toast.Provider swipeDirection="center">
-            <button
-                className={button.primary.lg}
-                onClick={() => {
-                    setOpen(false);
-                    window.clearTimeout(timerRef.current);
-                    timerRef.current = window.setTimeout(() => {
-                        eventDateRef.current = oneWeekAway();
-                        setOpen(true);
-                    }, 100);
-                }}
-            >
-                <span>Add to calendar</span>
-            </button>
-
-            <Toast.Root className={theme.root} open={open} onOpenChange={setOpen}>
-                <div className={theme.header}>
-                    <Toast.Title className={theme.title}>Scheduled: Catch up</Toast.Title>
-                    <div className={theme.actions}>
-                        <Toast.Action asChild altText="Goto schedule to undo">
-                            <button className={ghostButton.primary.sm}>
-                                <span>Undo</span>
-                            </button>
-                        </Toast.Action>
-                        <Toast.Close>
-                            <button className={ghostIconButton.gray.sm}>
-                                <span className="sr-only">Dismiss toast</span>
-                                <Cross2Icon className={ghostIconButton.icon.md} aria-hidden />
-                            </button>
-                        </Toast.Close>
-                    </div>
-                </div>
-                <Toast.Description asChild>
-                    <time className={theme.description} dateTime={eventDateRef.current.toISOString()}>
-                        {prettyDate(eventDateRef.current)}
-                    </time>
-                </Toast.Description>
-            </Toast.Root>
-            <Toast.Viewport className={theme.viewport} />
-        </Toast.Provider>
-    );
-};
-
-function oneWeekAway() {
-    const now = new Date();
-    const inOneWeek = now.setDate(now.getDate() + 7);
-    return new Date(inOneWeek);
+const Toast = {
+  Provider: ToastProvider,
+  Root: ToastRoot,
 }
 
-function prettyDate(date: Date) {
-    return new Intl.DateTimeFormat("en-US", { dateStyle: "full", timeStyle: "short" }).format(date);
-}
+export default Toast;
 
-export default ToastUI;
+export {
+  ToastProvider,
+  ToastRoot,
+}
