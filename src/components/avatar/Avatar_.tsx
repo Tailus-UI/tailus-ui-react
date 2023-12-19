4d52f1d02d38cd3f2a8d215dc0abdf08d18dfbeb
Théo Balick <balickmethens@gmail.com>
2023-12-19 10:57:11 +0100

    Refactor `Avatar` component and add `AvatarImage`
    This commit refactors the `Avatar` component by adding a new `AvatarImage` component. The `AvatarImage` component is responsible for rendering the image within the avatar. It receives the `className` prop for customization and has its HTML element reference forwarded. The `AvatarImage` component also utilizes the `AvatarContext` to determine the appropriate variant styling, and applies the corresponding variant class to the rendered image element. The overall structure of the `Avatar` component remains unchanged.
    
    `src/components/avatar/Avatar_.tsx`:
    - Refactor `Avatar` component by adding `AvatarImage` component
    - Export `AvatarImage`

--- src/components/avatar/Avatar_.tsx
+++ src/components/avatar/Avatar_.tsx
@@ -1,14 +1,14 @@
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
-import { cn } from '../../lib/utils';
+import {cn} from '../../lib/utils';
 import {
-    avatar as solidTheme,
-    softFallbackAvatar as softTheme
+  avatar as solidTheme,
+  softFallbackAvatar as softTheme
 } from "@tailus/themer-avatar";
 import React from "react";
 
 const variants = {
-    soft: softTheme,
-    solid: solidTheme
+  soft: softTheme,
+  solid: solidTheme
 };
 
 type AvatarVariant = "solid" | "soft";
@@ -16,76 +16,92 @@ type Intent = "primary" | "secondary" | "accent" | "danger" | "success" | "warni
 type Status = "online" | "offline" | "away" | "busy";
 type Size = "xs" | "sm" | "md" | "lg" | "xl";
 type AvatarProps = {
-    variant: AvatarVariant,
-    intent: Intent,
-    status: Status,
-    size:Size
+  variant: AvatarVariant,
+  intent: Intent,
+  status: Status,
+  size: Size
 }
 
 const AvatarContext = React.createContext<AvatarProps>({
-    variant: "solid",
-    intent: "primary",
-    size: "md",
-    status: "online",
+  variant: "solid",
+  intent: "primary",
+  size: "md",
+  status: "online",
 });
 
 interface AvatarRootProps {
-    isSoft?: boolean,
-    size?: Size,
-    intent?: Intent,
-    Status?: Status
+  isSoft?: boolean,
+  size?: Size,
+  intent?: Intent,
+  Status?: Status
 }
 
 const AvatarRoot = React.forwardRef<
-    React.ElementRef<typeof AvatarPrimitive.Root>,
-    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> & AvatarRootProps
->(({className,...props}, ref) => {
-    const { isSoft, size } = props;
-    const contextValues = React.useContext(AvatarContext);
-    const variant = isSoft ? "soft" : contextValues.variant;
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> & AvatarRootProps
+>(({className, ...props}, ref) => {
+  const {isSoft, size} = props;
+  const contextValues = React.useContext(AvatarContext);
+  const variant = isSoft ? "soft" : contextValues.variant;
 
-    const updatedContextValues = {
-        ...contextValues,
-        variant: variant
-    };
+  const updatedContextValues = {
+    ...contextValues,
+    variant: variant
+  };
 
-    return (
-        <AvatarContext.Provider value={updatedContextValues}>
-            <AvatarPrimitive.Root
-                {...props}
-                ref={ref}
-                className={cn(variants[variant].root[size || contextValues.size], className)}
-            />
-        </AvatarContext.Provider>
-    );
+  return (
+    <AvatarContext.Provider value={updatedContextValues}>
+      <AvatarPrimitive.Root
+        {...props}
+        ref={ref}
+        className={cn(variants[variant].root[size || contextValues.size], className)}
+      />
+    </AvatarContext.Provider>
+  );
 });
 
 type AvatarFallbackProps = {
-    intent?: Intent,
+  intent?: Intent,
 }
 
 const AvatarFallback = React.forwardRef<
-    React.ElementRef<typeof AvatarPrimitive.Fallback>,
-    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback> & AvatarFallbackProps
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback> & AvatarFallbackProps
 >(({className, ...props}, ref) => {
-    const { intent, variant } = React.useContext(AvatarContext);
-    return (
-        <AvatarPrimitive.Fallback
-            {...props}
-            ref={ref}
-            className={cn(variants[variant].fallback[intent], className)}
-        />
-    );
+  const {intent, variant} = React.useContext(AvatarContext);
+  return (
+    <AvatarPrimitive.Fallback
+      {...props}
+      ref={ref}
+      className={cn(variants[variant].fallback[intent], className)}
+    />
+  );
+});
+
+const AvatarImage = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({className, ...props}, ref) => {
+  const {variant} = React.useContext(AvatarContext);
+  return (
+    <AvatarPrimitive.Image
+      {...props}
+      ref={ref}
+      className={cn(variants[variant].image, className)}
+    />
+  );
 });
 
 const Avatar = {
-    Root: AvatarRoot,
-    Fallback: AvatarFallback,
+  Root: AvatarRoot,
+  Fallback: AvatarFallback,
+  Image: AvatarImage,
 }
 
 export default Avatar;
 
 export {
-    AvatarRoot,
-    AvatarFallback,
+  AvatarRoot,
+  AvatarFallback,
+  AvatarImage,
 }
