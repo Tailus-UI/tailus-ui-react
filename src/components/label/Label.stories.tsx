7cd8a3f410a5bcc6e2c37df554d9e5bde93eb53c
Théo Balick <balickmethens@gmail.com>
2023-12-21 11:06:01 +0100

    Refactor Label component and add new Horizontal variant

--- src/components/label/Label.stories.tsx
+++ src/components/label/Label.stories.tsx
@@ -1,9 +1,36 @@
-import LabelUI from "./Label";
-import LabelHorizontal from "./Horizontal";
+import {FormInput as Input} from "../form";
+import Label from "./Label.tsx";
+
+const LabelUI = () => (
+    <div className="space-y-2">
+        <Label htmlFor="firstName">First name</Label>
+        <Input
+            type={"text"}
+            variant={"soft"}
+            size={"md"}
+            name={"firstName"}
+            id={"firstName"}
+            defaultValue={"Théo Balick"}
+        />
+    </div>
+    );
 
 export default {
     component: LabelUI,
 };
 
-export const Label = () => <LabelUI />;
-export const Horizontal = () => <LabelHorizontal />;
+export const Label_ = () => <LabelUI />;
+
+export const Horizontal = () => (
+    <div className="flex items-center gap-4">
+        <Label htmlFor="firstName" className="whitespace-nowrap">First name</Label>
+        <Input
+            type={"text"}
+            variant={"soft"}
+            size={"md"}
+            name={"firstName"}
+            id={"firstName"}
+            defaultValue={"Meschack Irung"}
+        />
+    </div>
+);
