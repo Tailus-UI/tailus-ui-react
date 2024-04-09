5e42bc70cda29af35eb4227d522da944a53ad47e
Meschac Irung <meschacirung@gmail.com>
2024-04-09 17:51:27 +0200

    update : rebuild Checkbox component with new themer

--- src/components/checkbox/Checkbox.stories.tsx
+++ src/components/checkbox/Checkbox.stories.tsx
@@ -1,19 +1,23 @@
+import { CheckIcon, DividerHorizontalIcon } from "@radix-ui/react-icons";
 import {CheckboxIndicator, CheckboxLabel, CheckboxRoot} from "./Checkbox";
 import {Meta, StoryObj} from "@storybook/react";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
+import React from "react";
 
-const CheckboxUI = ({ variant, children, label }: { variant: 'default'|'ring', children: ReactNode, label: string }) => (
-  <div className="flex items-center">
-    <CheckboxRoot variant={variant} defaultChecked={true} id="c1">
-      <CheckboxIndicator>
-        {children}
-      </CheckboxIndicator>
-    </CheckboxRoot>
-    <CheckboxLabel htmlFor="c1">
-      {label}
-    </CheckboxLabel>
-  </div>
-);
+const CheckboxUI = ({ intent, label, fancy }: { intent: 'primary' | 'gray' | 'neutral', fancy?:boolean, children: ReactNode, label: string }) => {
+  return (
+    <div className="flex items-center">
+      <CheckboxRoot intent={intent} id="c1" fancy={fancy} defaultChecked>
+        <CheckboxIndicator>
+          <CheckIcon className="size-5" />
+        </CheckboxIndicator>
+      </CheckboxRoot>
+      <CheckboxLabel htmlFor="c1" className="text-gray-950 dark:text-white peer-disabled:opacity-50">
+        {label}
+      </CheckboxLabel>
+  </div> 
+  )
+};
 
 const meta: Meta<typeof CheckboxUI> = {
   title: 'Checkbox',
@@ -28,10 +32,13 @@ const meta: Meta<typeof CheckboxUI> = {
   },
   tags: ['autodocs'],
   argTypes: {
-    variant: {
+    intent: {
       control: 'select',
-      options: ['default', 'ring'],
-      defaultValue: 'default',
+      options: ['primary', 'gray', 'neutral'],
+      defaultValue: 'primary',
+    },
+    fancy: {
+      control: 'boolean',
     },
     children: {
       control: 'hidden'
@@ -48,32 +55,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Checkbox: Story = {
   args: {
-    variant: 'default',
+    intent: 'primary',
     label: 'Keep me signed in',
-    children: <svg className="w-4 h-4" viewBox="0 0 46 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clipPath="url(#clip0_23_2)">
-        <g filter="url(#filter0_d_23_2)">
-          <path d="M6 15.91L10.4241 11.546L19.2713 20.273L34.7547 5L39.1788 9.364L19.2723 29L6 15.91Z"
-                fill="white"/>
-        </g>
-      </g>
-      <defs>
-        <filter id="filter0_d_23_2" x="2" y="4" width="41.1788" height="32" filterUnits="userSpaceOnUse"
-                colorInterpolationFilters="sRGB">
-          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                         result="hardAlpha"/>
-          <feOffset dy="3"/>
-          <feGaussianBlur stdDeviation="2"/>
-          <feComposite in2="hardAlpha" operator="out"/>
-          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.4 0"/>
-          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_23_2"/>
-          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_23_2" result="shape"/>
-        </filter>
-        <clipPath id="clip0_23_2">
-          <rect width="46" height="33" fill="white"/>
-        </clipPath>
-      </defs>
-    </svg>,
   },
 };
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -2,32 +2,52 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import {checkbox as checkboxTheme, ringVariant as checkBoxRingTheme} from "@tailus/themer-checkbox";
 import React, {forwardRef} from "react";
 import {cn} from "../../lib/utils";
-import {cva, type VariantProps} from "class-variance-authority";
 
-export type CheckboxVariantProps = VariantProps<typeof checkboxVariants>;
-const checkboxVariants = cva('',{
+import { tv, type VariantProps } from "tailwind-variants"
+
+const checkbox = tv({
+  base: "border border-[--form-border-color] shadow-sm group rounded peer flex justify-center items-center size-5 text-white outline-2 outline-primary-600 outline-offset-2 text-white hover:brightness-95 focus-visible:outline dark:bg-gray-500/10 data-[state=checked]:border-none data-[state=indeterminate]:border-none disabled:bg-gray-100 dark:disabled:bg-gray-800 disabled:opacity-50 disabled:border-gray-300 dark:disabled:border-gray-700 disabled:shadow-none disabled:data-[state=checked]:bg-gray-300 dark:disabled:data-[state=checked]:bg-gray-700 dark:disabled:data-[state=indeterminate]:bg-gray-800 disabled:data-[state=checked]:shadow-none disabled:data-[state=indeterminate]:bg-gray-300 disabled:data-[state=indeterminate]:shadow-none",
   variants: {
-    variant: {
-      default: checkboxTheme.root,
-      ring: checkBoxRingTheme.root,
+    intent: {
+      primary: "data-[state=checked]:bg-primary-600 data-[state=indeterminate]:bg-primary-600",
+      gray: "data-[state=checked]:bg-gray-600 data-[state=indeterminate]:bg-gray-600",
+      neutral: "data-[state=checked]:bg-gray-950 dark:data-[state=checked]:bg-white data-[state=indeterminate]:bg-gray-950 dark:data-[state=indeterminate]:bg-white dark:text-gray-950 dark:disabled:text-white",
     }
-  }
-});
+  },
+})
+
+const fancyCheckbox = tv({
+  extend: checkbox,
+  base: "dark:[--radial-opacity:0] data-[state=checked]:[--radial-opacity:0.3] data-[state=checked]:[box-shadow:rgba(255,255,255,0.2)_0px_1px_0px_0px_inset,var(--checkbox-border-color)_0px_0px_0px_1px] data-[state=checked]:hover:brightness-[1.1] data-[state=checked]:[background-image:radial-gradient(76%_151%_at_52%_-52%,rgba(255,255,255,var(--radial-opacity))_0%,transparent_100%)] data-[state=indeterminate]:[--radial-opacity:0.3] data-[state=indeterminate]:[box-shadow:rgba(255,255,255,0.2)_0px_1px_0px_0px_inset,var(--checkbox-border-color)_0px_0px_0px_1px] data-[state=indeterminate]:hover:brightness-[1.1] data-[state=indeterminate]:[background-image:radial-gradient(76%_151%_at_52%_-52%,rgba(255,255,255,var(--radial-opacity))_0%,transparent_100%)]",
+  variants: {
+    intent: {
+      primary: "data-[state=checked]:[--checkbox-border-color:theme(colors.primary.600)] data-[state=indeterminate]:[--checkbox-border-color:theme(colors.primary.600)]",
+      gray: "data-[state=checked]:[--checkbox-border-color:theme(colors.gray.600)] data-[state=indeterminate]:[--checkbox-border-color:theme(colors.gray.600)]",
+      neutral: "data-[state=checked]:[--checkbox-border-color:theme(colors.gray.950)] dark:data-[state=checked]:text-gray-950 data-[state=indeterminate]:[--checkbox-border-color:theme(colors.gray.950)]",
+    }
+  },
+})
+
+export type CheckboxVariantProps = VariantProps<typeof checkbox>;
 
 export interface CheckboxProps extends CheckboxVariantProps {
   className?: string;
+  fancy?: boolean;
 }
 
 const CheckboxRoot = forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & CheckboxProps
->(({className, variant = 'default', ...props}: CheckboxProps, forwardedRef) => (
-  <CheckboxPrimitive.Root
-    ref={forwardedRef}
-    className={ cn(checkboxVariants({variant: variant}), className) }
-    {...props}
-  />
-));
+  >(({ className, intent, fancy, ...props }: CheckboxProps, forwardedRef) => {
+    const classes = fancy ? fancyCheckbox({ intent, className }) : checkbox({ intent, className });
+    return(
+      <CheckboxPrimitive.Root
+        ref={forwardedRef}
+        className={classes}
+        {...props}
+      />
+    )
+});
 
 type CheckboxIndicatorProps = {
   className?: string;
@@ -40,7 +60,7 @@ const CheckboxIndicator = forwardRef<
 >(({className, ...props}: CheckboxIndicatorProps, forwardedRef) => (
   <CheckboxPrimitive.Indicator
     ref={forwardedRef}
-    className={ cn(checkboxTheme.indicator, className) }
+    className=" "
     {...props}
   />
 ));
