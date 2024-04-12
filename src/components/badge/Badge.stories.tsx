ffeb55361f2be5ef64226286a1a4f46b19292244
Meschac Irung <meschacirung@gmail.com>
2024-04-12 18:48:07 +0200

    fix : Type 'undefined' cannot be used as an index type.

--- src/components/badge/Badge.stories.tsx
+++ src/components/badge/Badge.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Badge } from "./Badge";
 
-
 const meta: Meta<typeof Badge> = {
   title: 'Badge',
   component: Badge,
--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -10,7 +10,7 @@ export const Badge: React.FC<BadgeVariantsProps> = ({
     variant,
     ...props
   }) => {
-   
+    variant = variant || "solid";
     return(
         <span className={badge[variant]({intent, size})} {...props}>
             {children}
