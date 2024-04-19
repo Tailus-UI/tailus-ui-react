5f59eefdc3f5df7a7559279bd1b1ed21a2b81ac3
Meschac Irung <meschacirung@gmail.com>
2024-04-19 12:46:57 +0200

    test : build label examples

--- src/components/label/Label.stories.tsx
+++ src/components/label/Label.stories.tsx
@@ -1,26 +1,29 @@
-import {Meta} from "@storybook/react";
-import {FormInput as Input} from "../form";
+import { Meta } from "@storybook/react";
+import { FormInput as Input } from "../form";
 import Label from "./Label.tsx";
-import { outlinedForm } from "@tailus/themer-form";
+import { form } from "@tailus/themer"
 
 type LabelUIProps = {
-    size: "xs" | "sm" | "md"; 
+    size: "sm" | "md" | "lg" | "xl"; 
     disabled? : boolean
 };
 
+const { field } = form()
+
 export const LabelUI = ({size, disabled}:LabelUIProps) => (
-    <div className={outlinedForm.field + " w-72"}>
+    <div className={field({size:"md", class:"space-y-3"})}>
         <Label htmlFor="firstName" size={size}>First name</Label>
         <Input
             type="text"
-            size="md"
+            size="lg"
+            variant="mixed"
             name="firstName"
             id="firstName"
             defaultValue="Théo Balick"
             disabled={disabled}
         />
     </div>
-    );
+);
 
 const meta: Meta<typeof LabelUI> = {
     title: 'Label',
@@ -41,14 +44,15 @@ const meta: Meta<typeof LabelUI> = {
 export default meta;
 
 export const Horizontal = ({size, disabled}:LabelUIProps) => (
-    <div className={outlinedForm.field + " flex space-y-0 gap-4 items-center"}>
-        <Label htmlFor="firstName" size={size} className="whitespace-nowrap">First name</Label>
+    <div className={field({size:"md", class:"w-full flex items-center gap-3"})}>
+        <Label htmlFor="firstName2" size={size} className="text-nowrap">First name</Label>
         <Input
             type="text"
-            size="md"
-            name="firstName"
-            id="firstName"
-            defaultValue="Meschack Irung"
+            size="lg"
+            variant="outlined"
+            name="firstName2"
+            id="firstName2"
+            defaultValue="Théo Balick"
             disabled={disabled}
         />
     </div>
