978919f1947f45a1037371dd66d4b9908e09242e
Meschac Irung <meschacirung@gmail.com>
2024-04-22 15:01:20 +0200

    test : add story controls

--- src/components/drawer/Drawer.stories.tsx
+++ src/components/drawer/Drawer.stories.tsx
@@ -19,7 +19,7 @@ export default {
 
 export const Default = (args: DrawerProps) => (
 
-  <Drawer.Root direction={args.direction} shouldScaleBackground>
+  <Drawer.Root direction={args.direction} withControler={args.withControler} shouldScaleBackground>
     <Drawer.Trigger asChild>
         <Button.Root variant="outlined" intent="gray">
             <Button.Label>
@@ -40,36 +40,36 @@ export const Default = (args: DrawerProps) => (
                 </Text>
                 <Text className="mb-8">It uses <Link href="https://www.radix-ui.com/docs/primitives/components/dialog" target="_blank">Radix's Dialog primitive</Link> under the hood and is inspired by <Link href="https://twitter.com/devongovett/status/1674470185783402496" target="_blank">this tweet.</Link></Text>
               <Drawer.NestedRoot direction={args.direction} shouldScaleBackground>
-    <Drawer.Trigger asChild>
-        <Button.Root variant="outlined" intent="gray" size="sm">
-            <Button.Label>
-                Open Drawer
-            </Button.Label>
-        </Button.Root>
-    </Drawer.Trigger>
-    <Drawer.Portal>
-        <Drawer.Overlay className="dark:bg-gray-950/50" />
-        <Drawer.Content {...args}>
-            <div className="max-w-md mx-auto">
-                <Drawer.Title>Drawer for React</Drawer.Title> 
-                <Text className="my-3">
-                    This component can be used as a Dialog replacement on mobile and tablet devices. You can read about why and how it was built <Link href="https://emilkowal.ski/ui/building-a-drawer-component" target="_blank">here</Link>
-                </Text>
-                <Text className="mb-3">
-                  It comes unstyled and has gesture-driven animations, and is made by <Link href="https://emilkowal.ski" target="_blank">Emil</Link>
-                </Text>
-                <Text className="mb-8">It uses <Link href="https://www.radix-ui.com/docs/primitives/components/dialog" target="_blank">Radix's Dialog primitive</Link> under the hood and is inspired by <Link href="https://twitter.com/devongovett/status/1674470185783402496" target="_blank">this tweet.</Link></Text>
-                <Drawer.Close asChild>
+                <Drawer.Trigger asChild>
                     <Button.Root variant="outlined" intent="gray" size="sm">
                         <Button.Label>
-                            Close Drawer
+                            Open Drawer
                         </Button.Label>
                     </Button.Root>
-                    </Drawer.Close>
-                </div>
-        </Drawer.Content>
-    </Drawer.Portal>
-    </Drawer.NestedRoot>
+                </Drawer.Trigger>
+                <Drawer.Portal>
+                    <Drawer.Overlay className="dark:bg-gray-950/50" />
+                    <Drawer.Content {...args}>
+                        <div className="max-w-md mx-auto">
+                            <Drawer.Title>Drawer for React</Drawer.Title> 
+                            <Text className="my-3">
+                                This component can be used as a Dialog replacement on mobile and tablet devices. You can read about why and how it was built <Link href="https://emilkowal.ski/ui/building-a-drawer-component" target="_blank">here</Link>
+                            </Text>
+                            <Text className="mb-3">
+                              It comes unstyled and has gesture-driven animations, and is made by <Link href="https://emilkowal.ski" target="_blank">Emil</Link>
+                            </Text>
+                            <Text className="mb-8">It uses <Link href="https://www.radix-ui.com/docs/primitives/components/dialog" target="_blank">Radix's Dialog primitive</Link> under the hood and is inspired by <Link href="https://twitter.com/devongovett/status/1674470185783402496" target="_blank">this tweet.</Link></Text>
+                            <Drawer.Close asChild>
+                                <Button.Root variant="outlined" intent="gray" size="sm">
+                                    <Button.Label>
+                                        Close Drawer
+                                    </Button.Label>
+                                </Button.Root>
+                                </Drawer.Close>
+                            </div>
+                    </Drawer.Content>
+                </Drawer.Portal>
+              </Drawer.NestedRoot>
         </div>
         </Drawer.Content>
     </Drawer.Portal>
@@ -81,4 +81,5 @@ Default.args = {
   mixed: false,
   fancy: false,
   direction: "bottom",
+  withControler: false,
 };
